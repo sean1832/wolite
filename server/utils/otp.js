d2c7c94b3189ed822d8b35a410c20a726fe10064
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2025-03-05 00:31:41 +1100

    feat: enhance createOTP function to accept customizable algorithm, digits, and period parameters

--- server/utils/otp.js
+++ server/utils/otp.js
@@ -1,12 +1,12 @@
 const otp = require("otpauth");
 
-function createOTP(secret) {
+function createOTP(secret, algorithm = "SHA256", digits = 6, period = 30) {
   let totp = new otp.TOTP({
     issuer: "WOLITE",
     label: "WOLITE",
-    algorithm: "SHA256",
-    digits: 6,
-    period: 30,
+    algorithm: algorithm,
+    digits: digits,
+    period: period,
     secret: secret,
   });
   return totp;
